f5538324597d94abbb9c46a904a8de079f1eef02
clarck <zhiwei.zhou@ydcfo.com.cn>
2019-10-23 19:01:41 +0800

    首页

---
+++ platform/js/common.js
@@ -0,0 +1,28 @@
+$(function(){
+$(".head_nav li").hover(function(){
+  debugger
+   var line = $(this).find('.line');
+   var more = $(this).find('.more_list');
+   if(line.length>0){
+     line.stop().animate({
+opacity:1
+     })
+   } else if(more.length>0){
+more.show().stop().animate({
+opacity:1
+     },200)
+   }
+},function(){
+var line = $(this).find('.line');
+   var more = $(this).find('.more_list');
+   if(line.length>0){
+     line.stop().animate({
+opacity:0
+     })
+  } else if(more.length>0){
+more.stop().animate({
+opacity:0
+     },200).dely(200).hide();
+   }
+})
+});
